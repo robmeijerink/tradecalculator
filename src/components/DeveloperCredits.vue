a314a288e32a771677e2d5e30500f5b8a4be8708
robmeijerink <rob.meijerink@gmail.com>
2021-08-28 16:12:47 +0200

    credits

--- src/components/DeveloperCredits.vue
+++ src/components/DeveloperCredits.vue
@@ -1,8 +1,19 @@
 <template>
     <div class="flex justify-between md:w-3/4 lg:w-1/2 mx-auto">
-      <h1 class="text-white text-xs sm:text-sm">Trade Position Size Calculator</h1>
-      <p class="text-gray-200 text-xs sm:text-sm" title="This application was developed by Rob Meijerink">
-        Built By: <a class="block sm:inline" href="https://github.com/robmeijerink" target="_blank" rel="noreferrer noopener">Rob Meijerink</a>
-      </p>
+        <h1 class="text-white text-xs sm:text-sm">Trade Position Size Calculator</h1>
+        <div class="text-gray-200 text-xs sm:text-sm">
+            <p>
+                Built By:
+            </p>
+            <a class="block sm:inline" href="https://github.com/robmeijerink" target="_blank" rel="noreferrer noopener" title="Rob Meijerink: Lead Developer">
+                Rob Meijerink
+            </a>
+            <div title="Nick Jason Hagen: Developer">
+                Nick Jason Hagen
+            </div>
+            <div title="Michel van Linschooten: Graphic Designer">
+                Michel van Linschooten
+            </div>
+        </div>
     </div>
 </template>
